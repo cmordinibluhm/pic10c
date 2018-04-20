ed9976839f4a3eb8ff8b38150bf3e92204e81e67
Casey Mordini-Bluhm <caseyb108@gmail.com>
2018-04-20 11:11:41 -0700

    I added passing_fun.cpp

---
+++ passing_fun.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+//pass an option/flag to a function to set policy
+
+enum action_t {DOUBLE, SQUARE };
+
+void do_it ( std::vector<int>& v, action_t flag) {
+	if (flag == TRIPLE ){
+		v [i] *= 2;
+	}
+	else { 
+		v[i] *= v[i];
+	 }
+}
+
+
+
+int main () {
+
+
+
+	return 0; 
+}
