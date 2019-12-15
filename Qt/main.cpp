a19d163237994808cb37e02df088633e9c08fc6d
Casey Mordini-Bluhm <caseyb108@gmail.com>
2019-12-15 15:57:18 -0800

    Clean up before merge

---
+++ Qt/main.cpp
@@ -0,0 +1,19 @@
+#include <QApplication>
+// #include <QLabel>
+// #include <QPushButton>
+
+#include <QHBoxLayout>
+#include <QSpinBox>
+#include <QSlider>
+// #include <QCurveBall>  // Joking :-P [ Let's go, Dodgers] Not joking ;-)
+
+int main(int argc, char* argv[]){
+    QApplication app(argc,argv);
+
+    /**
+        QLabel* label = 
+            new QLabel("<h2><i>Hola</i>, <font color=red>Cu-te!</font></h2>");  
+        label->show();
+    **/
+
+}
---
+++ class_notes.md
@@ -0,0 +1,33 @@
+4/25/18
+
+files are made behind the scenes
+	.pro file
+
+	Makefile	find the linker, compiler, etc, to store recipes?
+
+	main.cpp
+
+
+
+pseudo c++ code (an entity will translate what we write into c++)
+
+
+#include <Qlabel>
+#include <>
+
+int main (int argc, char* argv[]) {
+QApplication app(argc,argv);
+
+
+
+
+QLabel* label = new QLabel("Hole, Cu-te");
+
+label->show();
+
+
+}
+
+
+qmake 
+
